96ee7b87c0c42cb038d59186d21927caea4c1d91
LiEAEX <31808597+LiEAEX@users.noreply.github.com>
2018-11-06 18:48:14 -0600

    Update index.php

--- source code/index.php
+++ source code/index.php
@@ -1,7 +1,7 @@
 <?PHP
     header("Content-Type: text/html; charset=utf8");
     
-    $conn = new mysqli("127.0.0.1", "root", "liqifan6", "anime");//connect to database
+    $conn = new mysqli("127.0.0.1", "root", "******", "anime");
     if(!$conn){
         echo 'can not connect: ' . mysql_error();
     }
@@ -9,7 +9,7 @@
     $pwd = $_POST['password'];
     $sql = "select password from user where username='{$uin}'";
     $result = $conn->query($sql);
-    $n = $result->fetch_row();//这里的$result是一个对，存到变量$n里面。
+    $n = $result->fetch_row();// 这里的$result是一个对，存到变量$n里面。
     if($uin !="" && $pwd !="")
 {
     if($n[0]==$pwd)
@@ -25,5 +25,5 @@
     {
     echo "Username or Password can not be null!<br /><a href = 'index.html'>Return</a>";
     }
-    $conn->close();//关闭数据库
+    $conn->close();
 ?>
